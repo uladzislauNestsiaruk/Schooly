a31a64d9591456984a013aa4f9b998bb75fa4a97
Vlad261280 <nesterukoleg9@gmail.com>
2021-08-26 14:32:43 +0300

    show Nickname

--- app/src/main/java/com/egormoroz/schooly/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/egormoroz/schooly/ui/profile/ProfileFragment.java
@@ -9,12 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.egormoroz.schooly.Callbacks;
 import com.egormoroz.schooly.FirebaseModel;
 import com.egormoroz.schooly.MainActivity;
 import com.egormoroz.schooly.R;
+import com.egormoroz.schooly.RecentMethods;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class ProfileFragment extends Fragment {
-    FirebaseModel firebaseModel;
+    FirebaseModel firebaseModel = new FirebaseModel();
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
     }
@@ -35,7 +37,15 @@ public class ProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         TextView nickname = view.findViewById(R.id.usernick);
         ///////////////////////// set nickname /////////////////////
-
+        firebaseModel.initAll();
+        RecentMethods.UserNickByUid(firebaseModel.getUser().getUid(),
+                firebaseModel,
+                new Callbacks.GetUserNickByUid() {
+                    @Override
+                    public void PassUserNick(String nick) {
+                        nickname.setText(nick);
+                    }
+                });
         //////////////////////////////////////////////////
         ImageView imageView = view.findViewById(R.id.settingsIcon);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +55,7 @@ public class ProfileFragment extends Fragment {
                 ((MainActivity)getActivity()).setCurrentFragment(SettingsFragment.newInstance());
             }
         });
+        ///////// I want GM on CF
         ImageView arrowtowardrobe = view.findViewById(R.id.arrowtowardrobe);
         arrowtowardrobe.setOnClickListener(new View.OnClickListener() {
 
