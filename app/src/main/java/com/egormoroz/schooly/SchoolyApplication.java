1808d68b318d32939e2aa6e4c3cb0ab898daa828
ttyomaa6 <81015461+ttyomaa6@users.noreply.github.com>
2021-11-09 19:00:07 +0300

    nontifications start

--- app/src/main/java/com/egormoroz/schooly/SchoolyApplication.java
+++ app/src/main/java/com/egormoroz/schooly/SchoolyApplication.java
@@ -3,11 +3,14 @@ package com.egormoroz.schooly;
 import android.app.Application;
 import android.content.Intent;
 
+import com.egormoroz.schooly.ui.main.NontificationService;
+
 public class SchoolyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        startService(new Intent(this, NontificationService.class));
         startService(new Intent(this, SchoolyService.class));
     }
 }
---
+++ app/src/main/java/com/egormoroz/schooly/ui/main/NontificationService.java
@@ -0,0 +1,114 @@
+package com.egormoroz.schooly.ui.main;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.egormoroz.schooly.Callbacks;
+import com.egormoroz.schooly.FirebaseModel;
+import com.egormoroz.schooly.R;
+import com.egormoroz.schooly.RecentMethods;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+
+public class NontificationService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    FirebaseModel firebaseModel=new FirebaseModel();
+    private static final int NOTIFY_ID = 101;
+    private static final String CHANNEL_ID = "Tyomaa channel";
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        createNotificationChannel();
+        nontifcations();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void nontifcations() {
+        Intent intent = new Intent(this, MainFragment.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_schoolycoin)
+                .setContentTitle("tyomaa")
+                .setContentText("hello")
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFY_ID, builder.build());
+        Log.d("######", "good");
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.chanel_name);
+            String description = getString(R.string.app_name);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        createNotificationChannel();
+        nontifcations();
+    }
+
+    public void getChangesInSubscribers(){
+        RecentMethods.UserNickByUid(firebaseModel.getUser().getUid(), firebaseModel, new Callbacks.GetUserNickByUid() {
+            @Override
+            public void PassUserNick(String nick) {
+                Query query=firebaseModel.getUsersReference().child(nick).child("subscribers");
+                query.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+    }
+}
